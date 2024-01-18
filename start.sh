e53b6b4a2668619717d966c1aea3eca4180b008c
Julian Insua <jii.julious@gmail.com>
2024-01-18 12:50:58 +0100

    It was all an empty space after a variable in secret manager !@# (#18)

--- start.sh
+++ start.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-if [ -f "/app/app.env" ]; then
-  # Source and export variables
-  source "app.env"
-  export $(cut -d= -f1 /app/app.env)
-  echo "Variables from app.env have been sourced and exported."
-else
-  echo "Error: app.env file not found."
-fi
-
 echo "Run migrations"
+source /app/app.env
 /app/goose/bin/goose -dir /app/migration postgres "$DB_SOURCE" up -v
 
 echo "Start the app"
