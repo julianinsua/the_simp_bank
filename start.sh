b2efa248c44db9534114f67e8f1f37b24568ca3b
Julian Insua <jii.julious@gmail.com>
2024-01-18 09:59:12 +0100

    is it debugging if you don't get rid of the bug? (#12)
    * here we go again
    
    * is it really debugging if you don't get rid of the bug?

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ set -e
 
 echo "Run migrations"
 source /app/app.env
-echo $DB_DRIVER
-/app/goose/bin/goose -dir /app/migration postgres $DB_SOURCE up -v
+/app/goose/bin/goose -dir /app/migration postgres "$DB_SOURCE" up -v
 
 echo "Start the app"
 /app/main 
