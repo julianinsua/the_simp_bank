5f8ee930ac3db1d68aae8b30365625b789e459de
Julian Insua <jii.julious@gmail.com>
2024-01-18 12:01:06 +0100

    added changes sugested by chatgpt

--- start.sh
+++ start.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-set -a
-. /app/app.env 
-set +a
+if [ -f "/app/app.env" ]; then
+  # Source and export variables
+  source "app.env"
+  export $(cut -d= -f1 /app/app.env)
+  echo "Variables from app.env have been sourced and exported."
+else
+  echo "Error: app.env file not found."
+fi
 
 echo "Run migrations"
 /app/goose/bin/goose -dir /app/migration postgres "$DB_SOURCE" up -v
