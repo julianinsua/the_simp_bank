7d3ba1444eb118fa09f43dcbd663aafb0fa04663
Julian Insua <jii.julious@gmail.com>
2024-01-18 11:31:34 +0100

    third time is the charm (#14)

--- start.sh
+++ start.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-echo "Run migrations"
 set -o allexport
 source /app/app.env 
 set +o allexport
+
+echo "Run migrations"
 /app/goose/bin/goose -dir /app/migration postgres "$DB_SOURCE" up -v
 
 echo "Start the app"
