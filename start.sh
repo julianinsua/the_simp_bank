fb41121a76c40a57c66a20e2fce5cbabdfe8dc57
Julian Insua <jii.julious@gmail.com>
2024-01-18 10:17:11 +0100

    Trying to source that env file (#13)

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@
 set -e
 
 echo "Run migrations"
-source /app/app.env
+set -o allexport
+source /app/app.env 
+set +o allexport
 /app/goose/bin/goose -dir /app/migration postgres "$DB_SOURCE" up -v
 
 echo "Start the app"
